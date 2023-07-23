5a8df997dcf88f3268e2eceeb05443bed75dc95b
Chi Chun Chang <amosccchang@gmail.com>
2023-07-23 20:55:41 +0800

    docs: add run.sh to trigger docker building

---
+++ run.sh
@@ -0,0 +1,11 @@
+# Set the image name
+IMAGE_NAME="my_java_app"
+
+# Get the absolute path to the script's directory
+SCRIPT_DIRECTORY=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+
+# Build the Docker image
+docker build -t $IMAGE_NAME $SCRIPT_DIRECTORY
+
+# Run the test cases in the Docker container
+docker run -it --rm $IMAGE_NAME mvn -Dtest=LoginTest,LoginE2ETest,WebScrapingTest test
